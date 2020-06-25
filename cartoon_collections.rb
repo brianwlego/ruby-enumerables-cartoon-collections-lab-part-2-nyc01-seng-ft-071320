ee8e71d351449e9cca0bbaf744cd0c2c8b363aab
Brian Lego <legobw@gmail.com>
2020-06-25 16:17:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |matching|
-    matching == "Earth!"
+    matching == valid_calls.each 
     
     end
 end
