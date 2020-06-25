f0bf997c32ac72028d29ff82c178d34dd21e3f5b
Brian Lego <legobw@gmail.com>
2020-06-25 16:02:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,6 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   
-  planeteer_calls.find_all { |same| same
+  planeteer_calls.find { |same| same == valid_calls }
   
 end
