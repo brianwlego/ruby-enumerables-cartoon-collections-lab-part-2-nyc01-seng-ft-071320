c7447272ff14d35ec41702aee51fb789f5323c3a
Brian Lego <legobw@gmail.com>
2020-06-25 16:51:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |matching|
-    matching == valid_calls[0],
+    matching == valid_calls[0]
     matching == valid_calls[1]
     end
 end
